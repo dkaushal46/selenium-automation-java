c845bfa4f7f6513d34c05b5ed741d0e4940a99e8
unknown <dikshanshnath@gmail.com>
2025-09-28 11:27:31 +0530

    assignemt 3 complete

--- Section41_All_Codes/A21_ASSIGNMENT3.java
+++ Section41_All_Codes/A21_ASSIGNMENT3.java
@@ -1,20 +1,22 @@
 package demo;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class A21_ASSIGNMENT3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		WebDriver driver =new ChromeDriver();
+		WebDriver driver =new EdgeDriver();
 		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -33,7 +35,31 @@ public class A21_ASSIGNMENT3 {
 		dropdown.selectByVisibleText("Consultant");
 		
 		driver.findElement(By.id("terms")).click();
+		driver.findElement(By.id("signInBtn")).click();
 		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("Checkout")));
+		
+		List<WebElement> addToCarts = driver.findElements(By.xpath("//button[@class='btn btn-info']"));
+		for(int i=0;i<addToCarts.size();i++) {
+			WebElement addToCart =addToCarts.get(i);
+			addToCart.click();
+			
+		}
+		driver.findElement(By.cssSelector(".nav-link.btn.btn-primary")).click();
+		
+		driver.findElement(By.className("btn-success")).click();
+		
+		driver.findElement(By.id("country")).sendKeys("India");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='India']")));
+		driver.findElement(By.xpath("//a[text()='India']")).click();
+		
+		driver.findElement(By.xpath("(//label[@for='checkbox2'])[1]")).click();
+		driver.findElement(By.cssSelector(".btn.btn-success.btn-lg")).click();
+		
+		Thread.sleep(5000);
+		System.out.println(driver.findElement(By.cssSelector(".alert.alert-success.alert-dismissible")).getText());
+		
+		driver.quit();
 		
 	}
 
