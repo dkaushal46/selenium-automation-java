d8be551b8125abdcf38515f22256e7be74ea7f21
unknown <dikshanshnath@gmail.com>
2025-09-29 23:27:38 +0530

    windowHandler

---
+++ Section41_All_Codes/A23_WindoeHandelConcept.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class A23_WindoeHandelConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://www.rahulshettyacademy.com/loginpagePractise/");
+		driver.findElement(By.cssSelector(".blinkingText")).click();;
+	}
+
+}
