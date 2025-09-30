c567a3a5c6c1e8550a83dfac8fa6c84ae9b3c8ec
unknown <dikshanshnath@gmail.com>
2025-09-30 23:56:29 +0530

    updated

--- Section41_All_Codes/A23_WindoeHandelConcept.java
+++ Section41_All_Codes/A23_WindoeHandelConcept.java
@@ -1,5 +1,8 @@
 package demo;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +13,21 @@ public class A23_WindoeHandelConcept {
 		// TODO Auto-generated method stub
 		WebDriver driver =new ChromeDriver();
 		driver.get("https://www.rahulshettyacademy.com/loginpagePractise/");
-		driver.findElement(By.cssSelector(".blinkingText")).click();;
+		driver.manage().window().maximize();
+		driver.findElement(By.cssSelector(".blinkingText")).click();
+		
+		Set<String> windows = driver.getWindowHandles(); //[parentid ;;  childid]
+		
+		
+		Iterator<String> it=windows.iterator();   
+		String Parentid = it.next();  // from outside to parent
+		String Childid = it.next();  //  from cparent to child
+		
+		
+		driver.switchTo().window(Childid);
+		
+		System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+		driver.quit();
 	}
 
 }
